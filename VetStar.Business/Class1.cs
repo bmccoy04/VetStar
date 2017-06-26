d9648b7af43cb60c6fe02f11108f44c547ffdcf6
bmccoy04 <bryanmccoy04@gmail.com>
2017-06-25 20:24:02 -0500

    added some basic structure

---
+++ VetStar.Business/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace VetStar.Business
+{
+    public class Class1
+    {
+    }
+}
---
+++ VetStar.Shared/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace VetStar.Shared
+{
+    public class Class1
+    {
+    }
+}
